7582c900efeb2cd168e4dd4ee326cdf23451ba08
Barbara Reis <tallytareis@gmail.com>
2018-01-29 01:11:14 +0000

    Teste Factor

---
+++ ci/application/views/dashboard.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="PT">
+  <head>
+    <title>MARCOS SPOLAOR</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="assets/css/main.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+  
+    </style>
+  </head>
+  <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+  
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="#myPage">Marcos Spolaor</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-center">
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#quemsomos">QUEM SOMOS</a></li>
+            <li><a href="#servicos">SERVIÇOS</a></li>
+            <li><a href="#">NOTICIAS</a></li>
+            <li><a href="#contato">CONTATO</a></li>
+            <li><a href="#">AREA DO CLIENTE</a></li>
+            <li><a href="#" class="fa fa-facebook img-circle"></a></li>
+      </div>
+            
+            
+          </ul>
+        </div>
+    </nav>
+    
+    <div id="home">
+      <div class="jumbotron text-center">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+     
+        
+          <div class="row banner1">
+            <div class="col-md-6">  
+            <p class="texto">
+              Procurando por uma <br><span>CONTABILIDADE?</span><br><b>Conheça nossos serviços</b></p>
+           <a href="#" class="btn btn-primary saibamais">Saiba mais<span class="glyphicon glyphicon-play"></span></a>
+            </div>
+              <div class="col-md-6 rodape-banner">  
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="assets/img/img-1.png" alt=" " class="img-responsive">
+                  </div>
+                  <div class="item">
+                    <img src="assets/img/img-2.png" alt=" ">
+                  </div>
+                </div>  
+              </div>
+          </div>
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev"></a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next"></a>
+        </div>
+       
+         <p class="tel">(13) <span>3361-4324</span><br>
+           Qualidade e confiança para sua empresa
+        </p>
+        <a href="#" class="btn btn-primary empresa">Abra sua empresa em poucos dias</a>
+      </div>
+    </div>
+  
+    <!-- Container Quem Somos -->
+    <div id="quemsomos" class="container-fluid sessao">
+      <h2>No que nossa 
+        <span>contabilidade</span> 
+      pode ajudar você?</h2><br>
+      <h4>Entender a necessidade da sua empresa, encontrar soluções inovadoras e compatíveis com o mercado, associados à <br>melhor solução técnica, faz parte da essência da Marcos Spolaor, uma empresa diferenciada e moderna, pois sempre <br>busca a satisfação do cliente na sua expectativa máxima.</h4><br>
+         <div class="bloco1">
+           <div class="row bloco2">
+             <div class="col-md-4 fa fa-caret-right"> 
+               Fornecer um serviço simpático, cortês e eficiente
+              </div>
+             <div class="col-md-4 fa fa-caret-right"> 
+               Ouvir o que você está dizendo
+             </div>
+            <div class="col-md-4 fa fa-caret-right"> 
+              Trabalhar pra que você pague o mínimo de imposto dentro da lei
+            </div>
+           </div>  
+          
+        
+           <div class="row bloco2">
+             <div class="col-md-4 fa fa-caret-right"> 
+               Sempre superar as suas expectativas
+              </div>
+             <div class="col-md-4 fa fa-caret-right">
+               Comunicar-se de forma rápida e segura
+             </div>
+             <div class="col-md-4 fa fa-caret-right"> 
+               Ser honesto, sincero e aberto com você o tempo todo
+              </div>
+           </div>
+         </div>
+    </div>
+    <!--CONTAINER SERVIÇOS -->
+    <div id="servicos" class="container-fluid text-center sessao servicos">
+      <h2>Nossos 
+        <span> serviços</span>
+      </h2>
+      <div class="row slideanim">
+        <div class="col-sm-4">
+          <span class="glyphicon glyphicon-list-alt logo-small"></span>
+          <h4>Contabilidade</h4>
+          <p>Nosso departamento contábil está pronto<br> a atender as necessidades legais da sua empresa.</p>
+        </div>
+        <div class="col-sm-4">
+          <span class="glyphicon glyphicon-check logo-small"></span>
+          <h4>Fiscal</h4>
+          <p>Sua empresa com uma acessoria completa<br> para atender a todas as obrigações fiscais.</p>
+        </div>
+        <div class="col-sm-4">
+          <span class="glyphicon glyphicon-home logo-small"></span>
+          <h4>Abertura de empresa</h4>
+          <p>Abre sua empresa com a estrutura societária mais <br>adequada, sempre com foco na economia tributária. </p>
+        </div>
+      </div>
+      <div class="row slideanim">
+        <div class="col-sm-4">
+          <span class="glyphicon glyphicon-tree-deciduous logo-small"></span>
+          <h4>Departamento Pessoal </h4>
+          <p>Imprescindível para qualquer empresa, sendo<br> responsável pela execução de toda a rotina de<br> departamento pessoal.</p>
+        </div>
+        <div class="col-sm-4">
+          <span class="glyphicon glyphicon-user logo-small"></span>
+          <h4>Societário</h4>
+          <p>Serviços completos em questões que envolvem a <br>parte legal (administrativa) das empresas junto<br> aos órgãos públicos e privados.</p>
+        </div>
+        <div class="col-sm-4">
+          <span class="glyphicon glyphicon-cog logo-small"></span>
+          <h4>Outsorcing</h4>
+          <p>Está buscando soluções específicas para o seu<br>negócio? Os melhores planos para o <br>seu ramo de atuação</p>
+        </div>
+      </div>
+    </div>
+    
+    <!--CONTAINER DIFERENCIAIS-->
+    <div class="banner2">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="assets/img/img-3.png" alt="Los Angeles" class=" img-2 img-responsive">
+            <a href="#" class="btn-primary fale">Fale Conosco</a>
+          </div>
+          <div class="col-md-4 accordion">  
+            <h2> Nossos diferenciais</h2>
+               
+            <div class="panel-group" id="accordion">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                  <span class="glyphicon glyphicon-comment"></span> Impostos em dia</a>
+                  </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse in">
+                  <div class="panel-body"> Nunca mais esqueça o vencimento das guias de impostos. A Marcos Spolaor disponibiliza as guias de impostos online, em tempo habil de pagamento e ainda te lembra do vencimento por e-mail e SMS</div>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                    <span class="glyphicon glyphicon-download"></span> Holerite online</a>
+                  </h4>
+                </div>
+                <div id="collapse2" class="panel-collapse collapse">
+                  <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  commodo consequat.</div>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                   <span class="glyphicon glyphicon-cloud"></span> Documentos na nuvem </a>
+                  </h4>
+                </div>
+                <div id="collapse3" class="panel-collapse collapse">
+                  <div class="panel-body"> Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  commodo consequat.</div>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+                   <span class="glyphicon glyphicon-file"></span> Imposto de renda</a>
+                  </h4>
+                </div>
+                <div id="collapse4" class="panel-collapse collapse">
+                  <div class="panel-body"> Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  commodo consequat.</div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>  
+  
+  <!--CONTAINER CONTATO-->
+    
+    <div id="contato" class="container-fluid ">
+      <div class="row rodape ">
+        <div class="col-md-6 news">
+          <h3> Receba Novidades</h3>
+          <p>Cadastre e receba no seu email as últimas novidades</p>
+          <form id="formulario" autocomplete="off">
+            <fieldset>
+              <input class="campo_email" type="email" placeholder=" Digite seu email" required ><br>
+              <button type="submit" class="btn btn-success">Quero me cadastrar  <span class="glyphicon glyphicon-play"></span></button>
+            </fieldset>
+          </form>
+        </div>
+        <div class="col-md-3 contato"> 
+          <h3>Contato</h3>
+          <p>Rua Ceará, 21, Sl 11, Vila Paulista <br> Cubatão, SP, 11510-050
+          <br>
+          <br>
+          (13) 3361- 4324</p>
+        </div>
+        <div class="col-md-3 facebook">
+          <h3>Siga nas redes sociais</h3>
+          <a href="#" class="fa fa-facebook img-circle"></a>
+        </div>
+      </div>
+      <div class="row footer"> 
+        <p>© 2017 - Marcos Spolaor - Organização contábil</p>
+      </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <script>
+    $(document).ready(function(){
+      // Add smooth scrolling to all links in navbar + footer link
+      $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 900, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+      
+      $(window).scroll(function() {
+        $(".slideanim").each(function(){
+          var pos = $(this).offset().top;
+    
+          var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+              $(this).addClass("slide");
+            }
+        });
+      });
+    })
+    
+    </script>
+  
+  </body>
+</html>
